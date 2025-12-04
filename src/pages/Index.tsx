7e372e400961c15a68bfcdce48b7dcf537bf5237
[dyad] <contact@rsmconseil.fr>
2025-12-04 10:50:36 +0100

    [dyad] Simplification: Hero + Footer uniquement - wrote 1 file(s), deleted 4 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,18 +1,10 @@
 import { Hero } from "@/components/Hero";
-import { ProblemSection } from "@/components/ProblemSection";
-import { SolutionSection } from "@/components/SolutionSection";
-import { HowItWorks } from "@/components/HowItWorks";
-import { UseCases } from "@/components/UseCases";
 import { Footer } from "@/components/Footer";
 
 const Index = () => {
   return (
-    <main className="bg-black min-h-screen text-white selection:bg-[#22c55e] selection:text-black">
+    <main className="bg-black min-h-screen text-white selection:bg-[#22c55e] selection:text-black flex flex-col justify-between">
       <Hero />
-      <ProblemSection />
-      <SolutionSection />
-      <HowItWorks />
-      <UseCases />
       <Footer />
     </main>
   );
