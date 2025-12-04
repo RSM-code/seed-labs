3051466cd402b50ee7b0fa188d0442d8eeaaebe7
[dyad] <contact@rsmconseil.fr>
2025-12-04 13:46:54 +0100

    [dyad] Adjusted hero section position - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -28,7 +28,7 @@ const Index = () => {
         {theme === "dark" ? <Sun size={20} /> : <Moon size={20} />}
       </button>
 
-      <div className="flex-grow flex flex-col justify-center items-center -translate-y-[150px]">
+      <div className="flex-grow flex flex-col justify-center items-center -translate-y-[100px]">
         <Hero />
       </div>
       <div className="flex-shrink-0">
